d297a2d7cd26347c68fe4aec8a4d535b7b0d4487
elimarmacena <elimar.macena@gmail.com>
2019-11-23 02:07:55 -0300

    UPDATE:alteracao da rota raiz para a hospedagem do heroku

--- public/assets/js/rotas.js
+++ public/assets/js/rotas.js
@@ -1,6 +1,6 @@
 
-//const rota_raiz = 'https://appet-back.herokuapp.com';
-const rota_raiz = "http://127.0.0.1:5001";
+const rota_raiz = 'https://appet-back.herokuapp.com';
+//const rota_raiz = "http://127.0.0.1:5001";
 const rota_user = rota_raiz+"/user";
 const rota_googleuser = rota_raiz+"/guser";
 const rota_current_user = rota_raiz+"/currentUser";
